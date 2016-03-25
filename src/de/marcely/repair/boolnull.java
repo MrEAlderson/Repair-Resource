b65fb88b54f1e0c26750d46edda750babbc87ae8
root <root@192109-909.pph-server.de>
2016-03-25 17:08:41 +0100

    Adding initial files.

---
+++ src/de/marcely/repair/boolnull.java
@@ -0,0 +1,22 @@
+package de.marcely.repair;
+
+public enum boolnull {
+	True,
+	False,
+	NULL;
+	
+	public boolean toBoolean(){
+		if(this == True)
+			return true;
+		
+		return false;
+	}
+	
+	public static boolnull valueOf(boolean bool){
+		if(bool == true)
+			return True;
+		else if(bool == false)
+			return False;
+		return NULL;
+	}
+}
